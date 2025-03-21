315812009fcbe84f656ea088cb50ad272147236a
Williams K A <104782325+kawilliams0@users.noreply.github.com>
2025-03-21 17:45:40 +0530

    Add files via upload
    java program to read  input from Scanner

---
+++ main.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+class main {
+    public static void main(String[] args) {
+        int id;
+		String name;
+		Scanner	sc=new Scanner(System.in);
+		System.out.println("Enter the id");
+		id=sc.nextInt();
+		sc.nextLine();
+		System.out.println("Enter the name");
+	    name=sc.nextLine();
+	    System.out.println("Id is "+id);
+		System.out.println("Name is "+name);
+		
+    }
+}
+    
+
